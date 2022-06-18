feba1876b072f69d4cbba5c3fbf140d287bc09a1
Jung Gyu Yoon <yjg30737@gmail.com>
2022-06-18 21:10:45 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-line-number-widget',
@@ -9,6 +16,8 @@ setup(
     packages=find_packages(),
     description='QWidget which shows line numbers of QTextBrowser or QTextEdit',
     url='https://github.com/yjg30737/pyqt-line-number-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
